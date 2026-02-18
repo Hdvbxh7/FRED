0aed249ac6257059cf5ac7e87017470baf3b7084
QuoicouLC <bbalc.olivier@gmail.com>
2026-02-18 17:44:40 +0100

    j'ai oublié de branch désolé

--- LibEvaluateur/EvaluationsCompilation/EvaluateurCompilation.java
+++ LibEvaluateur/EvaluationsCompilation/EvaluateurCompilation.java
@@ -1,7 +1,31 @@
 package LibEvaluateur.EvaluationsCompilation;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import LibEvaluateur.Evaluateur;
 
-public class EvaluateurCompilation extends Evaluateur {
+public abstract class EvaluateurCompilation extends Evaluateur {
+
+
+    protected List<File> fichiers = new ArrayList<File>();
+
+    public EvaluateurCompilation() {
+        super();
+    }
+
+    public String getResultat() {
+        return this.resultat;
+    }
+    
+    public Boolean[] getTestsResultat() {
+        return testsResultat;
+    }
+
+    protected abstract void resultatVersTAP(String SortieTest);
+
+    public abstract void evaluer() throws Exception;
+
     
 }
