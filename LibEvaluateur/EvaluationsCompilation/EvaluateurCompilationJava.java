2b8a97bbf23d5c5d1a494b1e424afcf2f0c78877
QuoicouLC <bbalc.olivier@gmail.com>
2026-02-25 10:39:41 +0100

    Je merge la branche*

--- LibEvaluateur/EvaluationsCompilation/EvaluateurCompilationJava.java
+++ LibEvaluateur/EvaluationsCompilation/EvaluateurCompilationJava.java
@@ -4,6 +4,7 @@ import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,8 +15,15 @@ public class EvaluateurCompilationJava extends EvaluateurCompilation {
 
     protected List<File> fichiers = new ArrayList<File>();
 
-    public EvaluateurCompilationJava() { 
+    public EvaluateurCompilationJava(File binaire) { 
         super();
+        fichiers.add(binaire);
+    }
+
+    public EvaluateurCompilationJava(File binaire, ArrayList<File> dependences) { 
+        super();
+        fichiers.add(binaire);
+        fichiers.addAll(dependences);
     }
 
     public String getResultat() {
@@ -28,9 +36,32 @@ public class EvaluateurCompilationJava extends EvaluateurCompilation {
 
     protected void resultatVersTAP(String SortieTest) {
 
+        System.out.println("Salut +" + SortieTest);
+
     }
 
     public void evaluer() throws Exception {
+
+        StringWriter sw = new StringWriter();
+        BufferedWriter out = new BufferedWriter(sw);
+
+        if (fichiers.size() > 1) {
+            String classpath = "";
+            for (int i = 1; i < fichiers.size(); i++) {
+                if (i > 1) {
+                    classpath += File.pathSeparator;
+                }
+                classpath += fichiers.get(i).getAbsolutePath();
+            }
+            ourJCompiler(out, fichiers.get(0).getAbsolutePath(), classpath);
+        } else {
+            ourJCompiler(out, fichiers.get(0).getAbsolutePath(), null);
+        }
+        out.close();
+        
+        String resultatsBruts = sw.toString();
+
+        resultatVersTAP(resultatsBruts);
 
     }
 
@@ -53,7 +84,8 @@ public class EvaluateurCompilationJava extends EvaluateurCompilation {
 
         JavaCompiler javac = ToolProvider.getSystemJavaCompiler();
         if(cp!=null){
-            javac.run(null, cout, cerr,"-d","LibEvaluateur/EvaluationsCompilation/Compiled_Code","-Xlint","-classpath",cp, file);
+            System.out.println(cp.toString());
+            javac.run(null, cout, cerr,"-d","LibEvaluateur/EvaluationsCompilation/Compiled_Code","-Xlint","-cp",cp, file);
         }else{
             javac.run(null, cout, cerr,"-d","LibEvaluateur/EvaluationsCompilation/Compiled_Code","-Xlint", file);
         }
@@ -69,14 +101,29 @@ public class EvaluateurCompilationJava extends EvaluateurCompilation {
 
     public static void main(String[] args) {
 
-        try { 
-            BufferedWriter out=new BufferedWriter(new FileWriter("log"));   
-            ourJCompiler(out, "LibEvaluateur/EvaluationsCompilation/Test.java", null);
+
+        try {
+            File aTester = new File("LibEvaluateur/EvaluationsCompilation/CompilationTest.java");
+            File junit = new File("libs/junit4.jar");
+            File junit2 = new File("libs/junit-jupiter-api-5.6.0.jar");
+            File junit3 = new File("libs/junit-platform-commons-1.6.0.jar");
+            File junit4 = new File("libs/junit-platform-engine-1.6.0.jar");
+            File junit5 = new File("libs/junit-platform-launcher-1.6.0.jar");
+            File apiguardian = new File("libs/apiguardian-api-1.1.0.jar");
+            ArrayList<File> libs = new ArrayList<File>();
+            libs.add(junit);
+            libs.add(junit2);
+            libs.add(junit3);
+            libs.add(junit4);
+            libs.add(junit5);
+            libs.add(apiguardian);
+            EvaluateurCompilationJava eval = new EvaluateurCompilationJava(aTester, libs);
+
+            eval.evaluer();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-
 
     }
     
